fe24419344f279fa0911c5872f353378985e4397
melbon <mario@vidov.it>
2015-10-24 10:09:50 +0300

    Added disabled state

--- jquery.cSelect.js
+++ jquery.cSelect.js
@@ -1,6 +1,6 @@
 /*
  * December 2013
- * customSelect 2.0.0
+ * customSelect 2.1.0
  * @author Mario Vidov
  * @url http://vidov.it
  * @twitter MarioVidov
@@ -8,20 +8,27 @@
  */
 $.fn.cSelect = function(options) {
     var settings = $.extend({
-        class: "cSelect"
+        class: "cSelect "
     }, options);
 
-    var container = this.selector,
-        wrapper = "<div class='" + settings.class + "' data-id='cSelect'></div>";
+    return this.each(function() {
+        var container = this,
+            id = $(this).attr("id") || "",
+            isDisabled =  "";
+        if ($(this).prop("disabled")) {
+            isDisabled = "select-is-disabled";
+        }
+        var wrapper = "<div class='" + settings.class + isDisabled + "' data-id='" + id + "' data-type='cSelect'></div>";
 
-    $(container).wrap(wrapper);
-    $(container).parent().prepend("<div />");
+        $(container).wrap(wrapper);
+        $(container).parent().prepend("<div />");
 
-    $(container).parent().find("div").text(function(){
-        return $(this).siblings('select').find('option:selected').text();
-    });
+        $(container).parent().find("div").text(function(){
+            return $(this).siblings('select').find('option:selected').text();
+        });
 
-    $(container).parent().find("select").on("change", function(){
-        $(this).siblings('div').text($(this).children('option:selected').text());
+        $(container).parent().find("select").on("change", function(){
+            $(this).siblings('div').text($(this).children('option:selected').text());
+        });
     });
 }
