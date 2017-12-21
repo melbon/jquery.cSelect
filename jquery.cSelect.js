bd4d854a730f5ec26f83ef491a14aeae4d7e7b61
Mario Vidov <mario@vidov.it>
2017-12-21 22:31:18 +0200

    Added first option and disabled attribute features

--- jquery.cSelect.js
+++ jquery.cSelect.js
@@ -8,7 +8,7 @@
  */
 $.fn.cSelect = function(options) {
     var settings = $.extend({
-        class: "cSelect "
+        class: "cSelect"
     }, options);
 
     return this.each(function() {
@@ -21,14 +21,31 @@ $.fn.cSelect = function(options) {
         var wrapper = "<div class='" + settings.class + isDisabled + "' data-id='" + id + "' data-type='cSelect'></div>";
 
         $(container).wrap(wrapper);
-        $(container).parent().prepend("<div />");
 
-        $(container).parent().find("div").text(function(){
+        var $parent = $(container).parent();
+        $parent.prepend("<div />");
+
+        $parent.find("div").text(function(){
             return $(this).siblings('select').find('option:selected').text();
         });
 
-        $(container).parent().find("select").on("change", function(){
+        $parent.find("select").on("change", function(){
             $(this).siblings('div').text($(this).find('option:selected').text());
         });
+
+        $(this).on('change', function() {
+            var firstOption = $('option', this).first();
+            var selectedOption = $('option:selected', this);
+            if ($(this).prop("disabled")) {
+                $parent.addClass("select-is-disabled");
+            } else {
+                $parent.removeClass("select-is-disabled");
+            }
+            if (firstOption.val() === selectedOption.val() && firstOption.text() === selectedOption.text()) {
+                $parent.addClass("first-option-selected");
+            } else {
+                $parent.removeClass("first-option-selected");
+            }
+        }).change();
     });
 }
